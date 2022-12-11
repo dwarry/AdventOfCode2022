150c6317ef539ed6613f0507b60dc3befc1241be
Dave Warry <dave@warry.org.uk>
2022-12-11 21:35:30 +0000

    Day 10 wip

---
+++ AdventOfCode2022/Day10.fs
@@ -0,0 +1,21 @@
+﻿module Day10
+
+type CpuState = { xRegister: int }
+
+type Instruction = {cyclesUntilCompletion: int; update: (CpuState -> CpuState)}
+
+let parseLine (line: string): Instruction = 
+    let parts = line.Split(' ')
+
+    match parts with
+    | [| "noop" |] -> {cyclesUntilCompletion = 1; update = id}
+    | [| "addx"; x |] -> {cyclesUntilCompletion = 3; update = fun cpu -> {cpu with xRegister = cpu.xRegister + (int x)}
+    | _ -> failwithf "Unexpected line: %s" line
+
+let rec processInstructions (instructions: Instruction list) (cpu: CpuState) = 
+    match instructions with
+    | [] -> cpu
+    | x::xs when x.cyclesUntilCompletion = 0 -> processInstructions xs (x.update cpu) 
+
+let run (path: string) =
+    ()
