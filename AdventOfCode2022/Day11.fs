9f46d4c97da6cce4a9587da2ce7bc5d36b5eb7b0
David Warry <dave.warry@live.com>
2022-12-19 06:30:18 +0000

    OK, so that's what "you'll need to find another way to keep your worry levels manageable" means. Gets stuck multiplying two bigints on round 96...

--- AdventOfCode2022/Day11.fs
+++ AdventOfCode2022/Day11.fs
@@ -2,20 +2,25 @@
 
 open Common
 
-type Monkey = {items: int64 array; operation: (int64->int64); test: (int64 -> bool); recipientIndexes: (int * int) }
+type Monkey = {
+        items: bigint array; 
+        operation: (bigint -> bigint); 
+        test: (bigint -> bool); 
+        recipientIndexes: (int * int) 
+    }
 
 let parseMonkey (lines: string list): Monkey = 
     let parseItems (s: string) = 
         let parts = s.Split(": ")[1]
-        parts.Split(", ") |> Array.map int64
+        parts.Split(", ") |> Array.map bigint.Parse
 
-    let parseOperation (s: string): (int64 -> int64) =
+    let parseOperation (s: string) =
         let parts = s.Split("= ")
         let [| x; op; y |] = parts[1].Split(' ')
 
         (fun v ->
-            let x2 = if x = "old" then v else (int64 x)
-            let y2 = if y = "old" then v else (int64 y)
+            let x2 = if x = "old" then v else (bigint.Parse x)
+            let y2 = if y = "old" then v else (bigint.Parse y)
             match op with
                 | "+" -> x2 + y2
                 | "-" -> x2 - y2
@@ -25,8 +30,8 @@ let parseMonkey (lines: string list): Monkey =
 
     let parseTest (s: string) = 
         let parts = s.Split("by ")
-        let denominator = int64 parts[1]
-        (fun (v: int64) -> (v % denominator) = 0)
+        let denominator = bigint (int parts[1])
+        (fun (v: bigint) -> (v % denominator) = bigint.Zero)
 
     let parseRecipient (s: string) = 
         let parts = s.Split("monkey ")
@@ -45,18 +50,20 @@ let parseMonkey (lines: string list): Monkey =
 
 let processRound (monkeys: Monkey array) (counts: int64 array) = 
     
+    let three = bigint 3
+
     let processMonkey (i: int) (monkey: Monkey) = 
                 
         Array.set counts i (counts[i] + (int64 (Array.length monkey.items)))
 
         monkey.items |>
             Array.iter (fun item -> 
-                            let newScore = ((monkey.operation item) / 3L)
+                            let newScore = ((monkey.operation item) / three)
                             let recipientIndex = if (monkey.test newScore) then (fst monkey.recipientIndexes) else (snd monkey.recipientIndexes)
                             let recipient = monkeys[recipientIndex] 
                             let newRecipient = {recipient with items = Array.append recipient.items [|newScore|] }
                             Array.set monkeys recipientIndex newRecipient
-                            let newMonkey = {monkey with items = Array.empty<int64>}
+                            let newMonkey = {monkey with items = Array.empty<bigint>}
                             Array.set monkeys i newMonkey)
 
     monkeys 
@@ -76,7 +83,7 @@ let processRound2 (monkeys: Monkey array) (counts: int64 array) =
                             let recipient = monkeys[recipientIndex] 
                             let newRecipient = {recipient with items = Array.append recipient.items [|newScore|] }
                             Array.set monkeys recipientIndex newRecipient
-                            let newMonkey = {monkey with items = Array.empty<int64>}
+                            let newMonkey = {monkey with items = Array.empty<bigint>}
                             Array.set monkeys i newMonkey)
 
     monkeys 
@@ -108,6 +115,7 @@ let run (path: string) =
     let counts2 = Array.create (Array.length monkeys) 0L
 
     for rnd in 1..10_000 do 
+        if rnd % 100 = 0 then printf "Round %d" rnd
         processRound2  monkeys2 counts2
     
     let part2 = 
